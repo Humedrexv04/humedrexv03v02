d9624772a711305a3ac327065857d984e3af514a
humedrexv04 <humedrexv04@gmail.com>
2025-04-21 13:00:22 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.humedrex.app',
+  appName: 'Humedrex',
+  webDir: 'dist/myapp/browser'
+};
+
+export default config;
